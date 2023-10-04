9135017245fdba7ec0449b859069a925908382c3
Gus Waldspurger <gus@waldspurger.com>
2023-10-04 18:21:29 -0500

    comment out cache release

--- csrc/ladcachemodule/ladcachemodule.c
+++ csrc/ladcachemodule/ladcachemodule.c
@@ -128,10 +128,10 @@ Request_dealloc(PyObject *self)
     DEBUG_LOG(SCOPE_INT, LOG_DEBUG, "Freeing %s at %p (%s) (repr = %p) (refcnt = %ld).\n", PyUnicode_AsUTF8(repr), self, r->request->path, repr, self->ob_refcnt);
     Py_DECREF(repr);
 
-    /* Release the wrapped request. */
-    if (r->request != NULL) {
-        cache_release(r->ustate, r->request);
-    }
+    // /* Release the wrapped request. */
+    // if (r->request != NULL) {
+    //     cache_release(r->ustate, r->request);
+    // }
 
     /* Release this object. */
     fprintf(stderr, "test test test 2222\n");
