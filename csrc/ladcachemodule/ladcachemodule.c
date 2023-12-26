49abf5f4d71b4fab28242ad6c29c284fdb3077e2
Gus Waldspurger <gus@waldspurger.com>
2023-12-26 15:29:26 -0800

    typo

--- csrc/ladcachemodule/ladcachemodule.c
+++ csrc/ladcachemodule/ladcachemodule.c
@@ -337,7 +337,7 @@ Cache_init(PyObject *self, PyObject *args, PyObject *kwds)
     }
     ARG_CHECK(capacity > 0, "capacity must be >= 1 byte", -1);
     ARG_CHECK(queue_depth > 0, "queue_depth must be > 0", -1);
-    ARG_CHECK(debug_limit > 0, "debug_limit must be > 0", -1);
+    ARG_CHECK(debug_limit >= 0, "debug_limit must be >= 0", -1);
     ARG_CHECK(max_unsynced >= 0, "max_unsynced must be >= 0", -1);
     ARG_CHECK(n_users >= 0, "n_users must be >= 0", -1);
 
